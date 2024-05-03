ccbf61d7fff596d6a1873edf81f3a1f5a7acc711
Nishu Goel <n.goel@epilot.cloud>
2024-05-03 17:29:44 +0530

    regen

--- examples/data-sources/epilot-schema_schema/data-source.tf
+++
@@ -1,4 +0,0 @@
-data "epilot-schema_schema" "my_schema" {
-  id   = "4c5d658b-093f-4118-abb4-2016edf4b44b"
-  slug = "contact"
-}
--- examples/resources/epilot_schema_schema_attribute/import.sh
+++
@@ -1,1 +0,0 @@
-terraform import epilot_schema_schema_attribute.my_epilot_schema_schema_attribute "contact:97644baa-083f-4e49-9188-fcff2ecaad7d"
